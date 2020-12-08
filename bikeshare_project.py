73c98e4334beade7f88a6677cdba82b151cb8119
Adriana Lopez <adriana.lopez-cordoba@eon.com>
2020-12-08 17:40:54 +0100

    Rephrased document string of get_filters function

--- bikeshare_project.py
+++ bikeshare_project.py
@@ -8,7 +8,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
 
 def get_filters():
     """
-    Asks user to specify a city, month, and day to analyze.
+    Asks customer to enter a city, a month, and a day to analyze.
 
     Returns:
         (str) city - name of the city to analyze
